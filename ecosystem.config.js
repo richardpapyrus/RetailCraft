cd1475e54c63564554174630b9ac5b81a34f60c5
mfonobongdev <mfonobongdev@gmail.com>
2026-02-09 22:50:17 +0100

    deployment fix 3

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,36 @@
 module.exports = {
     apps: [
         {
+            name: "pos-api-prod",
+            cwd: "./apps/api",
+            script: "node",
+            args: "dist/src/main.js",
+            env: {
+                NODE_ENV: "production",
+                PORT: 4000,
+                DATABASE_URL: "postgresql://doadmin:AVNS_iNZVQYj-RX036W3B0oS@retail-craft-db-postgres-do-user-24373738-0.k.db.ondigitalocean.com:25060/pos_db?sslmode=require"
+            }
+        },
+        {
+            name: "pos-web-prod",
+            cwd: "./apps/web",
+            script: "npm",
+            args: "run start",
+            env: {
+                NODE_ENV: "production",
+                PORT: 3000,
+                NEXT_PUBLIC_API_URL: "https://app.retailcraft.com.ng/api"
+            }
+        },
+        {
             name: "pos-api-staging",
             cwd: "./apps/api",
             script: "node",
             args: "dist/src/main.js",
-            env_staging: {
+            env: {
                 NODE_ENV: "production",
                 PORT: 4001,
                 DATABASE_URL: "postgresql://doadmin:AVNS_iNZVQYj-RX036W3B0oS@retail-craft-db-postgres-do-user-24373738-0.k.db.ondigitalocean.com:25060/pos_db_staging?sslmode=require"
-            },
-            env_development: {
-                NODE_ENV: "development",
-                PORT: 4000,
-                DATABASE_URL: "postgresql://admin:password@localhost:5432/pos_db?schema=public"
             }
         },
         {
@@ -21,15 +38,10 @@ module.exports = {
             cwd: "./apps/web",
             script: "npm",
             args: "run start",
-            env_staging: {
+            env: {
                 NODE_ENV: "production",
                 PORT: 3001,
                 NEXT_PUBLIC_API_URL: "https://staging.retailcraft.com.ng/api"
-            },
-            env_development: {
-                NODE_ENV: "development",
-                PORT: 3000,
-                NEXT_PUBLIC_API_URL: "http://localhost:4000"
             }
         }
     ]
